ca7c87d0967639d54f57d82fa0adf60fa1045288
Till Krüss <till@kruss.io>
2022-11-21 12:30:09 -0800

    add cluster boilerplate

---
+++ cluster.php
@@ -0,0 +1,29 @@
+<?php
+
+$nodes = array_map('trim', explode(',', getenv('REDIS_PRIMARIES')));
+
+if (empty(getenv('REDIS_AUTH'))) {
+    throw new Exception('No cluster password given');
+}
+
+$redis = new RedisCluster(
+    null,
+    $nodes,
+    1.0,
+    1.0,
+    false,
+    getenv('REDIS_AUTH')
+);
+
+// list and ping all masters
+foreach ($redis->_masters() as $master) {
+    var_dump(
+        $master,
+        $redis->ping($master)
+    );
+}
+
+// write to 20 hash slots
+foreach (range(1, 20) as $i) {
+    $redis->set("prefix:{{$i}}:foo", bin2hex(random_bytes(4)));
+}
